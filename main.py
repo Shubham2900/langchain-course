6402079a8d44b28d8a8baeee1d745b11ac7828f3
Shubham Bishnoi <shubhambishnoi8797@gmail.com>
2026-07-13 11:01:35 +0530

    Response Structuring

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
+from typing import List
+from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-tavily = TavilyClient()
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+    url:str = Field(description="The url of the source")
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool Searches over the internet
-    :param query: the query to search for
-    :return: The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query)
+class AgentResponse(BaseModel):
+    """ Schema for Response from the agent"""
+    answer: str = Field(description="The answer to the question")
+    sources: list[Source] = Field(default_factory=list, description="A list of sources used by the agent")
 
 llm = ChatOllama(temprature=0.5, model="gpt-oss:20b")
-tools = [search]
-agent = create_agent(llm, tools)
+tools = [TavilySearch()]
+agent = create_agent(llm, tools, response_format=AgentResponse)
 def main():
     print("Hello from langchain-course!")
     result = agent.invoke(
